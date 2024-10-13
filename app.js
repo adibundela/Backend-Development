0a1b3091bd1d242e9f75c6dca91284b45082d289
adibundela <adityabundela1505@gmail.com>
2024-10-13 14:56:00 +0530

    rest api with postman

--- app.js
+++ app.js
@@ -1,132 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
+///    routes in postmen 
 
-// app.get("/",(req,res)=>{
-    //     res.send(" in side / route")
-    // })
-    // app.get("/about",(req,res)=>{
-        //     res.send("in side / route")
-        // })
-        // app.get("*",(req,res)=>{
-            //     res.send("kuch bhi daloo yahi mikegaaa ")
-            // })
-            
-            // middleware ==> 
-                // every fun which have req,res,next in a perameter
-            
-            // jab bhi app browser se koi req backens pr bejhte h to bo apne route pr jati h agar app us req ko pahuchne se pahle kuch 
-            // check karna chahte h ya fir kuch jodna chahte h to middle ware use karte h 
-            
-            // Express-Session midilware 
-             const expressSession = require('express-session');
-             // connect flash middleware
-             const flash = require('connect-flash');
+var data= [2,3,4,5,6]
 
+app.get('/data',(req,res)=>{
+    res.send(data)
+})
 
-             // COOKIE PARSER 
-
-             const cookieParser = require('cookie-parser');
-
-
-
-
-             //   CORS middleware 
-             const cors = require('cors');
-             
-             
-             
-             //  MORGAN middleware 
-             const morgan  = require('morgan');
-             
-             // express-session middleware
-             
-             //  app.use(expressSession({
-                //     secret: "random stuff",
-                //     resave: false,       // for to not save when we did't change in session save
-                //     saveUninitialized: false  // when user is not logged in 
-                //  }))
-                //  app.get('/create',(req,res)=>{
-                //     req.session.polo = true;
-                //     res.send("done")
-                //  })
-                //  app.get('/check',(req,res)=>{
-                    //     console.log(req.session.polo);
-            //  })
-            
-            
-            
-            
-            
-            //    CONNECT FLASH MIDDLEWARE
-            
-            //  app.use(flash());
-            // app.get('/',(req,res)=>{
-            //     req.flash('error',"invalid credentials");
-            //     res.redirect('/error')
-            // })
-            // app.get('/error',(req,res)=>{
-                //     let massege =req.flash('error')
-                //     res.send(massege);
-                // })
-                
-                
-
-
-                //       CORS  
-                
-                
-                // app.use(cors());   // for all shareble routes
-
-                // app.get('/shareble',cors(),(req,res)=>{         if we want to make only one shareable route
-                //     res.send("done")
-                // })
-                
-                
-                //       cookie Parser 
-                // app.use(cookieParser())
-                // app.get('/banned',(req,res)=>{
-                    //     res.cookie("banned","True")
-                    //     res.send("banned")
-            // })
-            // app.get('/check',(req,res)=>{
-            //     console.log(req.cookies.banned);
-            //     res.send("done")
-            // })
-
-
-
-        //  MORGAN 
-
-            
-
-            // app.use(morgan("dev"))
-
-            // app.get('/',(req,res)=>{
-            //     res.send("Hello World")
-            // })
-
-
-
-        //       EJS 
-        
-        app.set('view engine', 'ejs')
-       
-
-        app.get('/',(req,res)=>{
-            res.render('index')
-        })
-        app.get('/check',(req,res)=>{
-            console.log(req.query);
-            res.send(req.query)
-        })
-        app.use(express.json())
-        app.use(express.urlencoded({extended:true}))
-        // to handle form with post request
-        app.post('/checkPost',(req,res)=>{
-            console.log(req.body);
-            res.send(req.body)
-        })
-
-
-        app.listen(3000);
+app.post('/data/:number',(req,res)=>{
+    console.log(typeof req.params.number);  // req.params.number is a string
+    console.log(req.params.number);
+    data.push(parseInt(req.params.number))  // so we have to convert it to a number and then push it
+    res.send(data)   
+})
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
---
+++ prev.js
@@ -0,0 +1,149 @@
+const express = require('express')
+const app = express()
+
+
+// app.get("/",(req,res)=>{
+    //     res.send(" in side / route")
+    // })
+    // app.get("/about",(req,res)=>{
+        //     res.send("in side / route")
+        // })
+        // app.get("*",(req,res)=>{
+            //     res.send("kuch bhi daloo yahi mikegaaa ")
+            // })
+            
+            // middleware ==> 
+                // every fun which have req,res,next in a perameter
+            
+            // jab bhi app browser se koi req backens pr bejhte h to bo apne route pr jati h agar app us req ko pahuchne se pahle kuch 
+            // check karna chahte h ya fir kuch jodna chahte h to middle ware use karte h 
+            
+            // Express-Session midilware 
+             const expressSession = require('express-session');
+             // connect flash middleware
+             const flash = require('connect-flash');
+
+
+             // COOKIE PARSER 
+
+             const cookieParser = require('cookie-parser');
+
+
+
+
+             //   CORS middleware 
+             const cors = require('cors');
+             
+             
+             
+             //  MORGAN middleware 
+             const morgan  = require('morgan');
+             
+             // express-session middleware
+             
+             //  app.use(expressSession({
+                //     secret: "random stuff",
+                //     resave: false,       // for to not save when we did't change in session save
+                //     saveUninitialized: false  // when user is not logged in 
+                //  }))
+                //  app.get('/create',(req,res)=>{
+                //     req.session.polo = true;
+                //     res.send("done")
+                //  })
+                //  app.get('/check',(req,res)=>{
+                    //     console.log(req.session.polo);
+            //  })
+            
+            
+            
+            
+            
+            //    CONNECT FLASH MIDDLEWARE
+            
+            //  app.use(flash());
+            // app.get('/',(req,res)=>{
+            //     req.flash('error',"invalid credentials");
+            //     res.redirect('/error')
+            // })
+            // app.get('/error',(req,res)=>{
+                //     let massege =req.flash('error')
+                //     res.send(massege);
+                // })
+                
+                
+
+
+                //       CORS  
+                
+                
+                // app.use(cors());   // for all shareble routes
+
+                // app.get('/shareble',cors(),(req,res)=>{         if we want to make only one shareable route
+                //     res.send("done")
+                // })
+                
+                
+                //       cookie Parser 
+                // app.use(cookieParser())
+                // app.get('/banned',(req,res)=>{
+                    //     res.cookie("banned","True")
+                    //     res.send("banned")
+            // })
+            // app.get('/check',(req,res)=>{
+            //     console.log(req.cookies.banned);
+            //     res.send("done")
+            // })
+
+
+
+        //  MORGAN 
+
+            
+
+            // app.use(morgan("dev"))
+
+            // app.get('/',(req,res)=>{
+            //     res.send("Hello World")
+            // })
+
+
+
+        //       EJS 
+        
+        // app.set('view engine', 'ejs')
+       
+
+        // app.get('/',(req,res)=>{
+        //     res.render('index')
+        // })
+        // app.get('/check',(req,res)=>{
+        //     console.log(req.query);
+        //     res.send(req.query)
+        // })
+        // app.use(express.json())
+        // app.use(express.urlencoded({extended:true}))
+        // // to handle form with post request
+        // app.post('/checkPost',(req,res)=>{
+        //     console.log(req.body);
+        //     res.send(req.body)
+        // })
+
+    //         ERROR HANDLING 
+
+    // app.get('/',(req,res,next)=>{
+    //     try{
+    //         res.send(hey)  //  hay is not defined so this throws error
+    //     }
+    //     catch(err){
+    //         next(err)
+    //     }
+
+    // })
+
+    // // error handler 
+
+    // app.use((err,req,res,next)=>{
+        
+    //     res.status(500).send(err.message)
+    // })
+        app.listen(3000);
